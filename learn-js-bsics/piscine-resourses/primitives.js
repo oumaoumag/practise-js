8a9e648b646ac8c98db2d82491101c24936e830a
oumouma <garveyshah1595@gmail.com>
2024-09-30 13:35:29 +0300

    directory for js recourses

---
+++ learn-js-bsics/piscine-resourses/primitives.js
@@ -0,0 +1,11 @@
+/*
+    Variables
+*/
+// A variable allow you to store any value at the given identifier
+// To achieve such feat we first need to declare our variable
+// We do that using the keyword `let` and then ick an identifier of our liking 
+let pouet
+console.log(pouet)
+// Now the identifier pouet can be use in the the rest of the code 
+// but we didn't give any value to our variable so showing it's value 
+// wll output undefined
