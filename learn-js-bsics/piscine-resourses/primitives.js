f403fa0faf1008b5739ad6f7d8353ab2550ed356
oumouma <garveyshah1595@gmail.com>
2024-09-30 15:41:21 +0300

    primities and variable decalaration

--- learn-js-bsics/piscine-resourses/primitives.js
+++ learn-js-bsics/piscine-resourses/primitives.js
@@ -84,18 +84,83 @@ console.log('aba' < 'abc') // = true
 
 // THE MORE YOU KNOW :
 
-// Non boolean values are either 'truthy' or falsy'
+// Non boolean values are either 'truthy' or 'falsy'
+console.log(!0) // = true
+console.log(!1) //= false
+console.log(!123) // = false
+console.log(!'') // true
+console.log(!'pouet') // = false
 
+// Type coercion is performed for comparisons with doubles equals...
+console.log('5' == 5) // = true
+console.log(null == undefined) // = true
 
+// ... it is recomended to use the strict === instead... 
+console.log('5' === 5) // = false
+console.log(null === undefined) // = false
+
+// JavaScript has one number type (which is a 64-bit IEEE 754 double) .
+// Doubles have a 52-bit mantissa, which is enough to store integers
+// up to about 9x10^15 precisely.
+
+// Float have limited precision, this is why :
+console.log(0.1 + 0.2) // is not precisly 0.3 but 0.30000000000000004
 
 /*
     Variables
 */
-// A variable allow you to store any value at the given identifier
+// A variable allow you to s`tore any value at the given identifier
 // To achieve such feat we first need to declare our variable
 // We do that using the keyword `let` and then ick an identifier of our liking 
 let pouet
-console.log(pouet)
 // Now the identifier pouet can be use in the the rest of the code 
 // but we didn't give any value to our variable so showing it's value 
 // wll output undefined
+console.log(pouet)
+
+// to assign a valur to it, we use a simple = 
+pouet = 42
+
+// Great, our variable identified by `pouet` now has the number 42 for value
+// let's see what is showed now :
+
+console.log(pouet)
+
+// I can re assign the value of my variable just like we did before :
+pouet = 'hehe'
+console.log(pouet) // THe same line of code now shows the new value
+
+// Valid identifiers can only contain: number, $, caps letter (A-Z),
+// normal letter (a-z) and underscore `_`
+let POUET_pouet // valid
+let pouet4 //  valid
+
+// but POUET-pouet and 4pouet would be invalid identifiers
+// in JavaScript, it's standard to use camelCase for identifier
+// since we can't use - as a seprator
+
+// You can not use a variable before its's declaration
+// You can do both the decalaration and the assignment in one line:
+
+let yo = 'lo'
+// Here,  we set the string value 'lo' to the variable identified by yo
+
+/* Constant variables
+*/ 
+
+// Since in most cases we don't want the value of our variables to change
+// It's better to use use constant variables, using 'const' instead of 'let'
+
+const isConstant = true
+
+// Here, we set the boolean true value
+// to the constant variabe identified by  'isConstant'
+
+// now we protect our variable against re-assignement
+
+// There's shorthand for performing math operatiion on variables:
+let someVar = 5
+someVar += 5 // equivalent to someVar = someVar + 5 someVar is 10 noW
+console.log(someVar)
+someVar *= 10 //  now someVar is 100
+console.log('someVar = ', someVar)
