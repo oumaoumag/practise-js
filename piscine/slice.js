9578086c962aa451425e93f8d1a9b9e09bbec5af
oumouma <garveyshah1595@gmail.com>
2024-10-05 01:43:21 +0300

    piscine clone

---
+++ piscine/slice.js
@@ -0,0 +1,9 @@
+// function that slices and array from, to the specified indexes
+function slice(input, start, end) {
+    //check if the input is a string or an array
+
+    if (typeof input === 'string') {
+        // Handle string slicing
+        // Normalize start and end indices
+    }
+}
