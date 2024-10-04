8a7ba3eed519848ca1118ff8d2beb5948957774c
oumouma <garveyshah1595@gmail.com>
2024-10-05 00:12:28 +0300

    array.slice

---
+++ learn-js-bsics/piscine-resourses/array.slice.js
@@ -0,0 +1,13 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'element'];
+
+console.log(animals.slice(2));
+
+console.log(animals.slice(2, 4));
+
+console.log(animals.slice(1, 5));
+
+console.log(animals.slice(-2));
+
+console.log(animals.slice(2, -1));
+
+console.log(animals.slice())
