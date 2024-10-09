d47d124136dc42bf423d4b55aeccb72cdd4aca00
oumouma <garveyshah1595@gmail.com>
2024-10-09 15:31:22 +0300

    output-to-cli

--- node-js/output-to-cli.js
+++ node-js/output-to-cli.js
@@ -4,4 +4,27 @@
 
 console.log('My name is %s and am %d year old.', 'Godwin', 26)
 
-console.log('%o', Number);
+// console.log('%o', Number);
+
+// Clear Elements
+// console.clear()
+
+// Counting Elements
+const x = 1;
+const y = 2;
+const z = 3;
+
+// console.count('The value of x is ' + x + ' and has been checked ..how many times?');
+// console.count('The value of y is ' + y + ' and has been checked ..how many times?');
+// console.count('The value of z is ' + z + ' and has been checked ..how many times?');
+
+const oranges = ['orange', 'orange'];
+const apples  = ['just one apple'];
+
+// oranges.forEach(fruit => {
+//     console.count(fruit);
+// });
+
+apples.forEach(fruit => {
+    console.count(fruit);
+});
