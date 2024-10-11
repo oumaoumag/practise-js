e7e2783147250d8c8c96ac5b0d5f13a1bf4b1e76
oumouma <garveyshah1595@gmail.com>
2024-10-11 23:43:37 +0300

    pasring a json string into an object

--- 100daycodingchallange/week-1-2:Fundementals-of-javascript/day8/json.js
+++ 100daycodingchallange/week-1-2:Fundementals-of-javascript/day8/json.js
@@ -1,2 +1,8 @@
 const obj = JSON.parse('{"name": "John", "age":30,"city":"New York"}');
-console.log(obj.name) 
+console.log(obj.name);
+
+// Second Example
+const obj2 = JSON.parse('{"result":true, "count":42}');
+console.log(obj2.count);
+
+console.log(obj2.result);
